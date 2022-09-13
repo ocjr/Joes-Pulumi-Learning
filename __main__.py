0e7a94e9039bf1f4f3dbf26be947ec9b8749e3ad
Joe O'Connell <github@bravoindia.net>
2022-09-12 22:50:58 -0700

    Reinititialized pulumi

--- __main__.py
+++ __main__.py
@@ -3,9 +3,16 @@
 import pulumi
 import pulumi_linode
 
+from pulumi_docker import Image, DockerBuild
+
+
+
 # Create a Linode resource (Linode Instance)
-instance = pulumi_linode.Instance('my-instance', type='g6-nanode-1', region='us-east',
-                                  image='linode/ubuntu18.04')
+instance = pulumi_linode.Instance('my-instance', 
+                                type='g6-nanode-1', 
+                                region='us-east', 
+                                image='linode/ubuntu18.04',
+                                root_pass='pulumi_test')
 
 # Export the Instance label of the instance
 pulumi.export('instance_label', instance.label)
